72aa67192663b213e91d93f876606234e4d08daf
Lee Mander <lee.mander@gmail.com>
2023-10-23 15:02:18 +0100

    Add styles

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,9 +5,18 @@ export default function Gallery() {
       <HornedBeast
         name="Highland Cow"
         url="https://images.unsplash.com/photo-1603104147308-73891c6be078"
-        desc="The Highland is a Scottish breed of rustic cattle. It originated in the Scottish Highlands and the Western Islands of Scotland and has long horns and a long shaggy coat."
+        desc="The Highland (Scottish Gaelic: Bò Ghàidhealach; Scots: Heilan coo) is a Scottish breed of rustic cattle. It originated in the Scottish Highlands and the Western Islands of Scotland and has long horns and a long shaggy coat."
       />
-      <HornedBeast />
+      <HornedBeast
+        name="Highland Cow"
+        url="https://images.unsplash.com/photo-1603104147308-73891c6be078"
+        desc="The Highland (Scottish Gaelic: Bò Ghàidhealach; Scots: Heilan coo) is a Scottish breed of rustic cattle. It originated in the Scottish Highlands and the Western Islands of Scotland and has long horns and a long shaggy coat."
+      />
+      <HornedBeast
+        name="Highland Cow"
+        url="https://images.unsplash.com/photo-1603104147308-73891c6be078"
+        desc="The Highland (Scottish Gaelic: Bò Ghàidhealach; Scots: Heilan coo) is a Scottish breed of rustic cattle. It originated in the Scottish Highlands and the Western Islands of Scotland and has long horns and a long shaggy coat."
+      />
     </section>
   );
 }
