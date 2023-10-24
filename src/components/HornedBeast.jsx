1a25d42de0055613aae09816b72b66af276253b2
Lee Mander <lee.mander@gmail.com>
2023-10-24 14:04:10 +0100

    Adjust styles

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -4,18 +4,19 @@ export default function HornedBeast(props) {
   const [faves, setFaves] = useState(0);
 
   function increaseFaves() {
-    console.log("hi");
     setFaves((previousFaves) => (previousFaves += 1));
   }
 
   return (
     <article className="gallery__beast">
-      <h2>{props.name}</h2>
+      <header>
+        <h2>{props.name}</h2>
+        <button className="beast__fav-button" onClick={increaseFaves}>
+          {faves}
+        </button>
+      </header>{" "}
       <img src={props.url} alt={props.name} />
       <p>{props.desc}</p>
-      <button className="beast__fav-button" onClick={increaseFaves}>
-        {faves}
-      </button>
     </article>
   );
 }
