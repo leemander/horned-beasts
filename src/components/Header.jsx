e1fcbdb5ef74af15f6c3d7cd7d8b19d0a30f52da
Lee Mander <lee.mander@gmail.com>
2023-10-24 14:22:21 +0100

    Add header interactivity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [styled, setStyled] = useState(false);
+
   return (
     <header>
-      <h1>Horned Beasts</h1>
+      <h1
+        onClick={() => setStyled((prev) => !prev)}
+        style={{ color: styled ? "hotpink" : "black" }}
+      >
+        Horned Beasts
+      </h1>
     </header>
   );
 }
