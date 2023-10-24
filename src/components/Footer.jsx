ccc2b186c10772227853ee22b7b7f607d681daa4
Lee Mander <lee.mander@gmail.com>
2023-10-24 14:43:42 +0100

    Add functions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,12 @@ import { useState } from "react";
 export default function Footer() {
   const [styled, setStyled] = useState(false);
 
+  function handleClick() {
+    setStyled((previous) => !previous);
+  }
+
   return (
-    <footer onClick={() => setStyled((prev) => !prev)}>
+    <footer onClick={handleClick}>
       <p className={styled ? "styled" : ""}>&copy; Lee Mander 2023</p>
     </footer>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,13 @@ import { useState } from "react";
 export default function Header() {
   const [styled, setStyled] = useState(false);
 
+  function handleClick() {
+    setStyled((previous) => !previous);
+  }
+
   return (
     <header>
-      <h1
-        onClick={() => setStyled((prev) => !prev)}
-        className={styled ? "styled" : ""}
-      >
+      <h1 className={styled ? "styled" : ""} onClick={handleClick}>
         Horned Beasts
       </h1>
     </header>
