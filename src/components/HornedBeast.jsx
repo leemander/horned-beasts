50bebd0db79731256ecb3f45af6befc998bee40d
Lee Mander <lee.mander@gmail.com>
2023-10-24 13:53:46 +0100

    Add favourite button and state

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 export default function HornedBeast(props) {
+  const [faves, setFaves] = useState(0);
+
+  function increaseFaves() {
+    console.log("hi");
+    setFaves((previousFaves) => (previousFaves += 1));
+  }
+
   return (
     <article className="gallery__beast">
       <h2>{props.name}</h2>
       <img src={props.url} alt={props.name} />
       <p>{props.desc}</p>
+      <button className="beast__fav-button" onClick={increaseFaves}>
+        {faves}
+      </button>
     </article>
   );
 }
