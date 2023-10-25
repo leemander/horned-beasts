4fbbff35b50eb055df25d5672452adc4ed32208d
Lee Mander <lee.mander@gmail.com>
2023-10-25 16:03:34 +0100

    Add keyword search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,13 @@ import Header from "./components/Header";
 import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
 import SelectedBeast from "./components/SelectedBeast";
+import SearchForm from "./components/SearchForm";
 
 function App() {
   const [chosenBeast, setChosenBeast] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredData, setFilteredData] = useState(null);
+
   function chooseBeast(name) {
     let newChosenBeast = {};
     data.forEach((beast) => {
@@ -20,11 +24,34 @@ function App() {
     setChosenBeast(newChosenBeast);
   }
 
+  function handleChange(e) {
+    setSearchTerm(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newFilteredData = data.filter((item) => {
+      if (item.keyword === searchTerm) {
+        return item;
+      }
+    });
+    setFilteredData(newFilteredData);
+  }
+
   return (
     <div className={chosenBeast ? "prevent-scrolling" : ""}>
       <Header />
       <main>
-        <Gallery data={data} chooseBeast={chooseBeast} />
+        <SearchForm
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          searchTerm={searchTerm}
+          setFilteredData={setFilteredData}
+        />
+        <Gallery
+          data={filteredData ? filteredData : data}
+          chooseBeast={chooseBeast}
+        />
       </main>
       <Footer />
       {chosenBeast && (
