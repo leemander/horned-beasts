af8772934baed9d2c1ffa6c984c0e340b8b526a1
Lee Mander <lee.mander@gmail.com>
2023-10-25 15:09:06 +0100

    Add ability to close modal by clicking background

--- src/components/SelectedBeast.jsx
+++ src/components/SelectedBeast.jsx
@@ -1,6 +1,12 @@
 export default function SelectedBeast({ setChosenBeast, url, name, desc }) {
+  function handleClick(e) {
+    if (e.target.className === "beast__modal") {
+      setChosenBeast(null);
+    }
+  }
+
   return (
-    <dialog className="beast__modal">
+    <dialog className="beast__modal" onClick={(e) => handleClick(e)}>
       <button aria-label="close modal" onClick={() => setChosenBeast(null)}>
         X
       </button>
